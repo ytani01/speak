6cd11e759c2702cc6824e2f51229e6b695e6af11
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2018-09-20 16:16:15 +0900

    update

--- speakipaddr.sh
+++ speakipaddr.sh
@@ -27,8 +27,11 @@ echo ${IPSTR1} ${IPSTR2} ${IPSTR3} ${IPSTR4}
 
 speak.sh ${STR1}
 speak.sh `echo ${IPSTR1} | sed 's/\([0-9]\)/\1 /g'`
+speak.sh "てん"
 speak.sh `echo ${IPSTR2} | sed 's/\([0-9]\)/\1 /g'`
+speak.sh "てん"
 speak.sh `echo ${IPSTR3} | sed 's/\([0-9]\)/\1 /g'`
+speak.sh "てん"
 speak.sh `echo ${IPSTR4} | sed 's/\([0-9]\)/\1 /g'`
 
 if [ X$1 = X ]; then
@@ -36,7 +39,10 @@ if [ X$1 = X ]; then
 fi
 speak.sh ${STR2}
 speak.sh `echo ${IPSTR1} | sed 's/\([0-9]\)/\1 /g'`
+speak.sh "てん"
 speak.sh `echo ${IPSTR2} | sed 's/\([0-9]\)/\1 /g'`
+speak.sh "てん"
 speak.sh `echo ${IPSTR3} | sed 's/\([0-9]\)/\1 /g'`
+speak.sh "てん"
 speak.sh `echo ${IPSTR4} | sed 's/\([0-9]\)/\1 /g'`
 exit
