d07b704280e54d9d51c04d29cb1fbea230d84161
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2018-09-27 13:03:52 +0900

    update

--- speakipaddr.sh
+++ speakipaddr.sh
@@ -2,47 +2,49 @@
 
 PATH="/home/pi/bin:${PATH}"
 
-STR0="IPアドレス 検出"
-STR1="読み上げます"
-STR2="繰り返します"
-ERRMSG="IPアドレスを検出できません"
+SPEAK_CMD="speak.py"
+
+MSG0="IPアドレス 検出"
+MSG1="読み上げます"
+MSG2="繰り返します"
+MSG_ERR="IPアドレスを検出できません"
+MSG_INTR="読上げを中断します"
+
+#####
+speak () {
+    for s in $*; do
+	s2=`echo $s | sed 's/\([0-9]\)/\1 /g'`
+	${SPEAK_CMD} "$s2"
+	if [ `gpio -g read 17` -eq 0 ]; then
+	    echo "!"
+	    ${SPEAK_CMD} ${MSG_INTR}
+	    exit 0
+	fi
+    done
+}
+
+##### main
 
 IPSTR=`hostname -I | grep '^[0-9]*\.[0-9]' | sed 's/ .*//'`
-echo $IPSTR
+echo ${IPSTR}
 if [ "X${IPSTR}" = "X" ]; then
-    speak.sh ${ERRMSG}
+    speak.sh ${MSG_ERR}
     exit 1
 fi
 
-speak.sh ${STR0}
+speak ${MSG0}
 
-IPSTR4=${IPSTR}
-IPSTR1=`echo ${IPSTR4} | sed 's/\..*$//'`
-IPSTR4=`echo ${IPSTR4} | sed 's/^[0-9]*\.//'`
-IPSTR2=`echo ${IPSTR4} | sed 's/\..*$//'`
-IPSTR4=`echo ${IPSTR4} | sed 's/^[0-9]*\.//'`
-IPSTR3=`echo ${IPSTR4} | sed 's/\..*$//'`
-IPSTR4=`echo ${IPSTR4} | sed 's/^[0-9]*\.//'`
-echo ${IPSTR1} ${IPSTR2} ${IPSTR3} ${IPSTR4}
+IPSTR2=`echo ${IPSTR} | sed 's/\./ てん /g'`
+echo ${IPSTR2}
 
-speak.sh ${STR1}
-speak.sh `echo ${IPSTR1} | sed 's/\([0-9]\)/\1 /g'`
-speak.sh "てん"
-speak.sh `echo ${IPSTR2} | sed 's/\([0-9]\)/\1 /g'`
-speak.sh "てん"
-speak.sh `echo ${IPSTR3} | sed 's/\([0-9]\)/\1 /g'`
-speak.sh "てん"
-speak.sh `echo ${IPSTR4} | sed 's/\([0-9]\)/\1 /g'`
+speak ${MSG1}
+speak ${IPSTR2}
 
 if [ X$1 = X ]; then
-  exit 0
+    exit 0
 fi
-speak.sh ${STR2}
-speak.sh `echo ${IPSTR1} | sed 's/\([0-9]\)/\1 /g'`
-speak.sh "てん"
-speak.sh `echo ${IPSTR2} | sed 's/\([0-9]\)/\1 /g'`
-speak.sh "てん"
-speak.sh `echo ${IPSTR3} | sed 's/\([0-9]\)/\1 /g'`
-speak.sh "てん"
-speak.sh `echo ${IPSTR4} | sed 's/\([0-9]\)/\1 /g'`
-exit
+
+speak ${MSG2}
+speak ${IPSTR2}
+
+exit 0
